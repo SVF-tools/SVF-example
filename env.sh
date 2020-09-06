65c2c19978061de3a144d1221918b1b2af2ee4fd
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-09-06 12:52:36 +0000

    update env

--- env.sh
+++ env.sh
@@ -4,11 +4,11 @@ PROJECTHOME=$(pwd)
 sysOS=`uname -s`
 LLVMHome="llvm-10.0.0.obj"
 install_path=`npm root`
-export LLVM_DIR=$install_path/$LLVMHome
+export LLVM_DIR=$HOME/$LLVMHome
 export PATH=$LLVM_DIR/bin:$PATH
 export PATH=$PROJECTHOME/bin:$PATH
-echo "export LLVM_DIR=$install_path/$LLVMHome" &>> ~/.bashrc
-echo "export PATH=$LLVM_DIR/bin:$PROJECTHOME/bin:$PATH" &>> ~/.bashrc
+echo "export LLVM_DIR=$HOME/$LLVMHome" >> ~/.bashrc
+echo "export PATH=$LLVM_DIR/bin:$PROJECTHOME/bin:$PATH" >> ~/.bashrc
 if [[ $sysOS == "Darwin" ]]
 then 
     export SVF_DIR=$install_path/SVF/
