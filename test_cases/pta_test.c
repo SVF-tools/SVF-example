7b789d3c3d359d2065eef71d1c7a419771db1724
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-10-08 12:43:48 +0000

    update pta test

--- test_cases/pta_test.c
+++ test_cases/pta_test.c
@@ -9,22 +9,10 @@ NOALIAS(a, c);
 
 int* y;
 
-// void src(){
-// }
-
-// void sink(){
-// }
-
 int main(){
 
     int* x = malloc(10);
     y = x;
     int* z = malloc(20);
     foo(x,y,z);
-    // int a = 1;
-    // while (a <= 1){
-    //     src();
-    //     a++;
-    // }
-    // sink();
 }
