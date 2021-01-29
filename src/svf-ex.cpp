43cc27ddba30aa88223d4f7b6fc244aa0eb15afb
rockysui@gmail.com <rockysui@gmail.com>
2021-01-29 13:01:57 +1100

    add dumpModule

--- src/svf-ex.cpp
+++ src/svf-ex.cpp
@@ -188,7 +188,7 @@ int main(int argc, char ** argv) {
 		saber->runOnModule(svfModule);
 	
 
-		LLVMModuleSet::getLLVMModuleSet()->dumpModulesToFile(".svf");
+		LLVMModuleSet::getLLVMModuleSet()->dumpModulesToFile(".svf.ll");
 
     return 0;
 }
