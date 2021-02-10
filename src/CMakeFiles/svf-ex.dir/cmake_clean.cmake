4277ce8954d21a1bfd40deec325a8cfec2e6b712
guanqin-123 <charles.guanqin@gmail.com>
2021-02-10 17:39:23 +1100

    VS Code configuration on pc

---
+++ src/CMakeFiles/svf-ex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/svf-ex"
+  "../bin/svf-ex.pdb"
+  "CMakeFiles/svf-ex.dir/svf-ex.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/svf-ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
